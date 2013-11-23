a5a3afe9c2ccece3a71217e0264a4ead5b840299
Victor Hogemann <victor.hogemann@gmail.com>
2013-11-23 20:57:50 -0200

    Test support

--- src/main/groovy/ws/deathmatch/service/DeathmatchService.groovy
+++ src/main/groovy/ws/deathmatch/service/DeathmatchService.groovy
@@ -3,10 +3,12 @@ package ws.deathmatch.service
 import org.springframework.stereotype.Service
 
 import ws.deathmatch.model.Board
+import ws.deathmatch.model.Player;
 import ws.deathmatch.model.Status;
 import ws.deathmatch.xml.DeathmatchRequest
 import ws.deathmatch.xml.DeathmatchResponse
 import ws.deathmatch.xml.CommandEnum as CMD
+import ws.deathmatch.xml.DeathmatchResponse.Player.Coords;
 
 @Service
 class DeathmatchService {
@@ -25,9 +27,21 @@ class DeathmatchService {
 		
 	}
 	
-	public Board kill( int playerId  ){
-		board.players.find{ it.id == playerId }?.status = Status.DEAD
-		return board
+	DeathmatchResponse insertCoin( DeathmatchRequest request ){
+		
+		Player player = board.newPlayer()
+		
+		DeathmatchResponse response = new DeathmatchResponse()
+		
+		response.player = new ws.deathmatch.xml.DeathmatchResponse.Player()
+		response.player.id = player.id
+		
+		response.player.coords = new Coords()
+		response.player.coords.x = player.position.x
+		response.player.coords.y = player.position.y
+		
+		return response
 	}
+	
 	
 }
---
+++ src/test/groovy/ws/deathmatch/service/DeathmatchServiceTest.groovy
@@ -0,0 +1,7 @@
+package ws.deathmatch.service
+
+class DeathmatchServiceTest {
+
+	
+	
+}
